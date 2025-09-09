dcc691a45e5bf16560d3367fc00d657d74a5ac6d
pedrobackes22 <167105235+pedrobackes22@users.noreply.github.com>
2025-09-09 10:42:13 -0300

    Add files via upload

---
+++ imersão1/dados.js
@@ -0,0 +1,63 @@
+let dados = [
+    {
+        titulo: "Lionel Messi",
+        descricao: "Lionel Messi é amplamente considerado um dos maiores jogadores de futebol de todos os tempos. Nascido em 24 de junho de 1987, em Rosario, Argentina, Messi se destacou desde jovem por seu talento excepcional com a bola. Ele é conhecido por sua habilidade técnica incomparável, visão de jogo e controle de bola quase sobrenatural.",
+        link: "https://pt.wikipedia.org/wiki/Lionel_Messi",
+        tags: "messi lionel",
+        imagem: "imagens/messi.webp"
+    
+    },
+    {
+        titulo: "LeBron James",
+        descricao: "LeBron James é um dos jogadores de basquete mais renomados e influentes da história da NBA. Nascido em 30 de dezembro de 1984, em Akron, Ohio, LeBron rapidamente se destacou como um prodígio do basquete desde seus anos de escola, e sua trajetória brilhante continuou ao longo de sua carreira profissional.",
+        link: "https://pt.wikipedia.org/wiki/LeBron_James",
+        tags: "LeBron James king",
+        imagem: "imagens/apks.png"
+    },
+    {
+        titulo: "Patrick Mahomes",
+        descricao: "Patrick Mahomes é um dos quarterbacks mais talentosos e excitantes da NFL, conhecido por sua habilidade de jogo excepcional e estilo inovador. Nascido em 17 de setembro de 1995, em Tyler, Texas, Mahomes ganhou destaque como um prodígio do futebol americano desde seus dias no ensino médio e na Universidade do Texas Tech.",
+        link: "https://pt.wikipedia.org/wiki/Patrick_Mahomes",
+        tags: "Patrick Mahomes",
+        imagem: "imagens/mahomes.png"
+    },
+    {
+        titulo: "Neymar",
+        descricao: "Neymar é um dos jogadores de futebol mais reconhecidos e talentosos do mundo. Nascido em 5 de fevereiro de 1992, em Mogi das Cruzes, São Paulo, ele começou sua carreira nas categorias de base do Santos, onde ganhou destaque por suas habilidades técnicas, dribles e velocidade",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "Neymar",
+        imagem: "imagens/backes.png"
+    },
+    {
+        titulo: "Kevin Durant",
+        descricao: "Kevin Durant é um dos jogadores de basquete mais talentosos e versáteis da NBA. Nascido em 29 de setembro de 1988, em Washington, D.C., ele se destacou desde jovem, mostrando habilidades excepcionais no basquete. Durant é conhecido por sua combinação de altura (cerca de 2,06 m) e habilidade de arremesso, o que o torna um marcador extremamente difícil de defender.",
+        link: "https://pt.wikipedia.org/wiki/Kevin_Durant",
+        tags: "KD",
+        imagem: "imagens/kd.png"
+        
+        
+    },
+    {
+        titulo: "Kylian Mbappé",
+        descricao: "Kylian Mbappé é um dos jogadores de futebol mais talentosos da sua geração. Nascido em 20 de dezembro de 1998, em Bondy, França, ele se destacou pela sua velocidade impressionante, habilidade técnica e visão de jogo.",
+        link:"https://pt.wikipedia.org/wiki/Kylian_Mbappé",
+        tags: "imagens/mbappe",
+        imagem: "imagens/250076574.png",
+
+    },
+    {
+        titulo: "Kyrie Irving",
+        descricao: "Kyrie Irving é um jogador de basquete profissional americano, conhecido por sua habilidade excepcional e estilo de jogo elétrico. Nascido em 23 de março de 1992, em Melbourne, Austrália, ele cresceu nos Estados Unidos e se destacou no basquete universitário pela Duke University antes de ser selecionado como a primeira escolha do draft da NBA de 2011 pelo Cleveland Cavaliers.",
+        link:"https://pt.wikipedia.org/wiki/Kyrie_Irving",
+        tags: "Kyrie Irving",
+        imagem: "imagens/baixados.png"
+    },
+    {
+        titulo: "Tyreek Hill",
+        descricao: "Tyreek Hill é um jogador de futebol americano, conhecido por sua velocidade explosiva e habilidades atléticas excepcionais. Ele atua como wide receiver e é famoso por seu papel como uma grande ameaça no jogo aéreo. Hill tem uma estatura mais baixa em comparação com muitos de seus colegas, mas sua agilidade e capacidade de fazer jogadas longas o tornam um dos recebedores mais temidos da liga.",
+        link:"https://pt.wikipedia.org/wiki/Tyreek_Hill",
+        tags: "Tyreek Hill",
+        imagem: "imagens/tyreek.png ",
+
+    },
+];
